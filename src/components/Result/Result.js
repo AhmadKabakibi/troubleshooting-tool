fdced7aea05fa416973b2c89502ccc123f9e8320
Ahmad Kabakibi <ahmadkbakibi@gmail.com>
2018-12-15 20:08:14 +0100

    add Result component

---
+++ src/components/Result/Result.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './Result.css'
+
+class Result extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="result">
+          Most likely root causes went through analysis Successfully
+        </h1>
+        <h2 className="result">we will inform you soon ...</h2>
+      </div>
+    )
+  }
+}
+
+export default Result
