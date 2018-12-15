b2b031d3e3d314d85e378cd3a2195467f37b8ebe
Ahmad Kabakibi <ahmadkbakibi@gmail.com>
2018-12-15 20:06:32 +0100

    add questions list in json formart

---
+++ src/constants/questions.js
@@ -0,0 +1,44 @@
+const questions = [
+  {
+    question: 'Sensor {id} not pinging?',
+    answers: ['yes', 'no', 'maybe']
+  },
+  {
+    question: 'Base station {id} not pinging?',
+    answers: ['yes', 'no', 'maybe']
+  },
+  {
+    question: 'Average data quality at farm {id} is low?',
+    answers: ['yes', 'no', 'maybe']
+  },
+  {
+    question: 'Average data quality for sensor {id} is low?',
+    answers: ['yes', 'no', 'maybe']
+  },
+  {
+    question: 'Field access point {id} has a very high load?',
+    answers: ['yes', 'no', 'maybe']
+  },
+  {
+    question: 'Average latency between fap {id} and bs {id} is high?',
+    answers: ['yes', 'no', 'maybe']
+  },
+  {
+    question: 'Sensor {id} has very low signal strength?',
+    answers: ['yes', 'no', 'maybe']
+  },
+  {
+    question: 'Fap {id} is not pinging to bs {id}?',
+    answers: ['yes', 'no', 'maybe']
+  },
+  {
+    question: 'All base stations not pinging?',
+    answers: ['yes', 'no', 'maybe']
+  },
+  {
+    question: 'All farms have low data quality?',
+    answers: ['yes', 'no', 'maybe']
+  }
+]
+
+export default questions
